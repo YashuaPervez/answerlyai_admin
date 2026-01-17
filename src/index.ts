1fec6fbf7d6c914215a09a5351ec7b70888da0d2
Yashua Pervez <yashuapervez@gmail.com>
2026-01-17 19:02:30 +0500

    fix: build

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-import serverless from '@vendia/serverless-express';
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import app from './app';
+import serverless from "serverless-http";
+import app from "./app";
 
-let serverlessExpressInstance: any;
-
-async function setup(event: APIGatewayProxyEvent, context: Context) {
-  serverlessExpressInstance = serverless({ app });
-  return serverlessExpressInstance(event, context);
-}
-
-function handler(event: APIGatewayProxyEvent, context: Context) {
-  if (serverlessExpressInstance) {
-    return serverlessExpressInstance(event, context);
-  }
-  return setup(event, context);
-}
-
-export { handler };
+export const handler = serverless(app);
